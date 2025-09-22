de5228fbdac855d70209240be83f741b9662208d
raees <mohitdamke1@gmail.com>
2025-09-22 23:06:51 +0530

    fix compile version

--- app/src/main/java/com/example/mybulkyee/MainActivity.kt
+++ app/src/main/java/com/example/mybulkyee/MainActivity.kt
@@ -1,17 +1,9 @@
 package com.example.mybulkyee
 
-import android.Manifest
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.Context
-import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.core.app.ActivityCompat
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import com.example.mybulkyee.navigation.NavigationControl
 import com.example.mybulkyee.ui.theme.BulkyeeTheme
 import com.google.android.gms.ads.MobileAds
